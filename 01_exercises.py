67d29686318ef0cc01405ac1374fe97dd6bde6ca
denisa-mat <96780077+denisa-mat@users.noreply.github.com>
2023-05-12 08:06:33 +0200

    Create 01_exercises.py

---
+++ 01_exercises.py
@@ -0,0 +1,8 @@
+from microbit import *
+
+/*
+Napište program, který na vestavěný displej vypíše řetězec "Hello World". Následně program nahrajte do micro:bitu.
+*/
+
+while True:
+    display.scroll("Hello World")
