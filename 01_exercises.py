c3663ac9b75d473f037d0e796132570a511cbc61
denisa-mat <96780077+denisa-mat@users.noreply.github.com>
2023-05-12 10:43:15 +0200

    Update 01_exercises.py

--- 01_exercises.py
+++ 01_exercises.py
@@ -1,8 +1,12 @@
-from microbit import *
 
 """
-Napište program, který na vestavěný displej vypíše řetězec "Hello World". Následně program nahrajte do micro:bitu.
+Úloha 1 - Hello World
+
+Napište program, který na vestavěný displej vypíše řetězec "Hello World". 
+Následně program nahrajte do micro:bitu.
 """
+
+from microbit import *
 
 while True:
     display.scroll("Hello World")
